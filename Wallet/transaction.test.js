2756fedc3332de465709b99cd95d9544832de8c2
Christian McKay Stevenson <mckay800@gmail.com>
2020-12-31 22:20:09 -0500

    Add files via upload

--- Wallet/transaction.test.js
+++ Wallet/transaction.test.js
@@ -23,6 +23,15 @@ describe('Transaction', () => {
         expect(transaction.input.amount).toEqual(wallet.balance);
     });
 
+    it('validates a valid transaction', () => {
+        expect(Transaction.verifyTransaction(transaction)).toBe(true);
+    });
+
+    it('invalidates a corrupt transaction', () => {
+        transaction.outputs[0].amount = 100000;
+        expect(Transaction.verifyTransaction(transaction)).toBe(false);
+    })
+
     describe('transacting with an amount that exceeds the balance', () => {
         beforeEach(() => {
             amount = 50000;
@@ -33,4 +42,22 @@ describe('Transaction', () => {
             expect(transaction).toEqual(undefined);
         });
     });
-});
+
+    describe('and updating a transaction', () => {
+        let nextAmount, nextRecipient;
+
+        beforeEach(() => {
+            nextAmount = 20;
+            nextRecipient = 'n3xt-4ddr355';
+            transaction = transaction.update(wallet, nextRecipient, nextAmount);
+        });
+
+        it(`subtracts the next amount from the sender's output`, () => {
+            expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount).toEqual(wallet.balance - amount - nextAmount);
+        });
+
+        it('outputs an amount for the next recipient', () => {
+            expect(transaction.outputs.find(output => output.address === nextRecipient).amount).toEqual(nextAmount);
+        });
+    });
+}); 
